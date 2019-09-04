553fb80f5e2a0eaeb782cd2e76a4bd465ad81c63
johnschneider1 <johnlawschneider@gmail.com>
2019-09-04 17:30:57 -0500

    working

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ server.get("/api/posts", (req, res) => {
       res.status(200).json(post);
     })
     .catch(err => {
+      console.log(err);
       res
         .status(500)
         .json({ message: "The post information could not be retrieved." });
@@ -31,7 +32,7 @@ server.get("/api/posts/:id/comments", (req, res) => {
   const postId = req.params.id;
   Posts.findPostComments(postId)
     .then(post => {
-      if (post) {
+      if (post.length > 0) {
         res.status(200).json(post);
       } else {
         res.status(404).json({
@@ -43,7 +44,10 @@ server.get("/api/posts/:id/comments", (req, res) => {
       res.status(500).json({ message: "err retriving comment" });
     });
 });
-// GET ID
+
+// get comments follow
+
+// GET ID destructure to return an object and not an array
 
 server.get("/api/posts/:id", (req, res) => {
   const postId = req.params.id;
@@ -62,6 +66,21 @@ server.get("/api/posts/:id", (req, res) => {
       res.status(500).json({ message: "err retriving post" });
     });
 });
+
+// follow get id
+
+// server.get("api.posts/:id", (req, res) => {
+//   const { id } = req.params;
+
+//   Posts.findById(id).then(([post]) => {
+//     console.log(post);
+//     if (post) {
+//       res.status(200).json(post);
+//     } else {
+//       res.status(404).json({ error: "post with id does not exist" });
+//     }
+//   });
+// });
 
 // methond delete
 server.delete("/api/posts/:id", (req, res) => {
@@ -93,65 +112,62 @@ server.post("/api/posts", (req, res) => {
       })
       .catch(err => {
         res.status(500).json({
-          message: "There was an error while saving the post to the database"
+          message: "there was an error while saving the post to the database"
         });
       });
   } else {
     res.status(400).json({
-      message: "Please provide a title and some content for the post."
+      message: "please provide a title and contents for the post"
     });
   }
 });
 
 // ADD COMMENT 	/api/posts/:id/comments
 
-server.post("	/api/posts/:id/comments", (req, res) => {
+server.post("/api/posts/:id/comments", (req, res) => {
   const rePost = req.body;
+  rePost.post_id = req.params.id;
+  if (rePost) {
+    Posts.insertComment(rePost)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "no luck" });
+      });
+  }
 });
+
+// method put update follow
+// server.put("/api/users/:id", (req, res))
 
 // METHOD UPDATE/PUT
 
-// server.put("/api/users/:id", (req, res) => {
-//   const { id } = req.params;
-//   const changes = req.body;
-
-//   Posts.update(id, changes)
-//     .then(post => {
-//       if (post) {
-//         res.status(200).json(post);
-//       } else {
-//         res
-//           .status(404)
-//           .json({ message: "The post with the specified ID does not exist." });
-//       }
-//     })
-//     .catch(error => {
-//       res
-//         .status(500)
-//         .json({ message: "Please provide a title and contents for the post" });
-//     });
-// });
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (changes.title && changes.contents) {
+    Posts.update(id, changes)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json(changes);
+        } else {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist" });
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "There was an error updating the post" });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide a title and contents for the post" });
+  }
+});
 
 module.exports = server;
-
-//  method update
-// server.put("/api/posts/:id", (req, res) => {
-//     const id = req.params.id:
-//     const update = req.body;
-//    Posts.update(id, update).then(updated => {
-//        if (updated) {
-//            Posts.findById(id)
-//            .then(post => {
-//                res.status(200).json({ message: "update working", post })
-//            })
-//            .catch(err => {
-//                res.send(500).json({ error: "user info not modded"})
-//            })
-//        } else {
-//            res.status(404).json({ message: "the users with specified id does not exist"})
-//        }
-//    })
-//    .catch(err => {
-//        res.status(500).json({ message: "the users info couldn't be modded"})
-//    })
-// })
